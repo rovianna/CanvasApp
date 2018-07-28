9948c724bc3c27b6665ed0bc8f11bd32b69161fa
Rodrigo Vianna <rodrigogvianna@outlook.com>
2018-07-28 20:30:53 -0300

    Basic init

---
+++ geometryApp/Controller/CanvasViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CanvasViewController.swift
+//  geometryApp
+//
+//  Created by Rodrigo Vianna on 28/07/18.
+//  Copyright © 2018 Rodrigo Vianna. All rights reserved.
+//
+
+import UIKit
+
+class CanvasViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
