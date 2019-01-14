eadd72c81a7537992d47d70fc653493b138c1ebb
Rodrigo Vianna <rodrigogvianna@outlook.com>
2019-01-14 11:46:31 -0200

    Square drawing

--- geometryApp/Models/Square.swift
+++ geometryApp/Models/Square.swift
@@ -10,7 +10,16 @@ import UIKit
 
 class Square: Geometry {
     static func drawForm(positionX: CGFloat, positionY: CGFloat) -> CAShapeLayer {
-        return CAShapeLayer()
+        let path = UIBezierPath(rect: CGRect(x: positionX - 10, y: positionY - 10, width: 20, height: 20))
+        
+        let shapeLayer = CAShapeLayer()
+        shapeLayer.path = path.cgPath
+        
+        shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.strokeColor = UIColor.red.cgColor
+        shapeLayer.lineWidth = 3.0
+        
+        return shapeLayer
     }
     
     var object: Form = .square
