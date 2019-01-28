63425fec3f74bae105a262ea942ec4f8358e4be1
Rodrigo Vianna <rodrigogvianna@outlook.com>
2019-01-28 10:39:39 -0200

    Added triangle drawing

--- geometryApp/Models/Triangle.swift
+++ geometryApp/Models/Triangle.swift
@@ -10,7 +10,19 @@ import UIKit
 
 class Triangle: Geometry {
     static func drawForm(positionX: CGFloat, positionY: CGFloat) -> CAShapeLayer {
-        return CAShapeLayer()
+        let layer = CAShapeLayer()
+        let path = CGMutablePath()
+        
+        path.move(to: CGPoint(x: positionX, y: positionY))
+        path.addLine(to: CGPoint(x: positionX + 20, y: positionY + 30))
+        path.addLine(to: CGPoint(x: positionX + 40, y: positionY))
+        path.addLine(to: CGPoint(x: positionX, y: positionY))
+        layer.strokeColor = UIColor.black.cgColor
+        layer.lineWidth = 3.0
+        layer.fillColor = UIColor.black.cgColor
+        layer.path = path
+        
+        return layer
     }
     
     var object: Form = .triangle
